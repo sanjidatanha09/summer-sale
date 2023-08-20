36b6c6fce6a9309f5419f2f5eef32c11e47e85ce
sanjidatanha09 <tanha.sanjida999@gmail.com>
2023-08-20 21:57:32 +0600

    added file

--- index.js
+++ index.js
@@ -1,30 +1,61 @@
-let total=0;
+let total = 0;
 
-function handleCLikBtn(target){
+function handleCLikBtn(target) {
 
-   // console.log(target.parentNode.childNodes);
+    // console.log(target.parentNode.childNodes);
     const selectedItemContainer = document.getElementById("selected-items");
     const itemName = target.parentNode.childNodes[1].innerText;
     const li = document.createElement("li");
     li.innerText = itemName;
-    //console.log(li);
+
+
     selectedItemContainer.appendChild(li);
 
     const price = target.parentNode.childNodes[3].innerText.split(" ")[0];
-
-    //console.log(price);
-    total =parseFloat(total) + parseFloat(price) ;
+    total = parseFloat(total) + parseFloat(price);
     document.getElementById("total").innerText = total;
-    //console.log(total);
 
+    const deletebutton = document.getElementById('submit-btn');
+    console.log(deletebutton);
 
+    if (total > 0) {
+        deletebutton.removeAttribute('disabled');
 
+    }
+    else {
+        deletebutton.setAttribute('disabled', true);
 
+    }
 
 }
 
+document.getElementById('input-field').addEventListener('keyup', function (event) {
+
+    const Sellproduct = event.target.value;
+    //console.log(text);
+    const deletebutton = document.getElementById('btn-update');
+    //&& totalprice > 200
+
+    if (Sellproduct === 'SELL200' && total > 200) {
+        deletebutton.removeAttribute('disabled');
 
 
-const submitbutton = document.getElementById('submit');
+        let discountamount = 0.2;
+        const totalDiscount = total * discountamount;
+        const discountElement = document.getElementById('discount');
+        discountElement.innerText = totalDiscount.toFixed(2);
 
-submit
+
+        const FinalTotal = total - totalDiscount;
+        const finalamount = document.getElementById('distotal');
+        finalamount.innerText = FinalTotal.toFixed(2);
+
+
+    }
+    else {
+        deletebutton.setAttribute('disabled', true);
+
+    }
+
+})
+
